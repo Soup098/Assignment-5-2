cf94c22d4d1ce033a8ecb614414895266eb71f63
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-02-06 20:04:21 -0700

    Add project files.

---
+++ Assignment 5-2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// create function to open FahrenheitTemperature.txt
+
+// create function that reads the the contents of that file as the input
+
+// create function that takes the tempurature readings from that file and converts them into celcius
+
+// create a new file called CelciusTemperature.txt and writes the converted temperature to the file.
+
+// close the file.
+
+// main function to run said functions.
